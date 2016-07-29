f33b4942b83cc632cb1221fc7d6017365bdba087
arpitjain06 <arpitjain06@hotmail.com>
2016-07-29 15:06:40 +0530

    change in query for item grid

--- hardware_store/customization/sales_invoice.py
+++ hardware_store/customization/sales_invoice.py
@@ -74,11 +74,11 @@ def get_items(price_list, sales_or_purchase, customer_group, item=None):
 
 	#end of original code
 
-	query = frappe.db.sql("""select i.name, i.item_name , i.image, reg.price_list_rate, 
+	query = frappe.db.sql("""select DISTINCT i.name, i.item_name , i.image, reg.price_list_rate, 
 		reg.uom, reg.minimum_qty  
 		from `tabItem`as  i LEFT JOIN 
 			(select parent , CONCAT(' ',uom_quantity, '-' ,TRUNCATE(rate, 2) ) as price_list_rate, min(minimum_qty) as minimum_qty, uom_quantity as uom  from 
-						`{child_table}` where minimum_qty !=0 group by uom_quantity) reg 
+						`{child_table}` where minimum_qty !=0 group by parent) reg 
 		ON 
 			(i.name = reg.parent or reg.parent=i.variant_of )
 		where
@@ -88,7 +88,7 @@ def get_items(price_list, sales_or_purchase, customer_group, item=None):
 			{order_by}
 			i.name
 		limit 24""".format(condition=condition, order_by=order_by, child_table=item_price_list), args, as_dict=1)
-
+	
 	price_acc_to_uom =frappe.db.sql("""select i.name as item_code, ifnull(CONCAT(' ',reg.uom_quantity, '-' , TRUNCATE(reg.rate,2)), 0.0) rate, reg.minimum_qty 
 			from `tabItem` i , `{0}` reg  
 			where 
@@ -98,11 +98,10 @@ def get_items(price_list, sales_or_purchase, customer_group, item=None):
 	c = {}
 	for d in price_acc_to_uom:
 		c.setdefault(d['item_code'], []).append(d['rate'])
-
+	
 	for i in query:
 		if i['item_name'] in c.keys():
 			i['price_list_rate'] = c[i['item_name']]
-
 	return query
 
 
